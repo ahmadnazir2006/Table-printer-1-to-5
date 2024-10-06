7fc1d6c19cec7b1e18865159e2e1cc61ad4c364b
ahmadnazir2006 <ahmadnazir9101@gmail.com>
2024-10-06 11:04:21 +0500

    Add files via upload

---
+++ tables print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int i = 1;
+	while (i <= 5) {
+		int j = 1;
+		while (j <= 10) {
+			cout << i * j<<endl;
+			j++;
+		}
+		cout <<endl;
+		i++;
+	}
+	return 0;
+}
